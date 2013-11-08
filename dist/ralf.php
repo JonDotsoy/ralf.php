9d4c3de97c580970815fade6c4a49ad192c2b60f
Jonathan <jonathan@jonathan-pc.(none)>
2013-11-08 17:14:48 -0300

    Carga de repositorio

--- dist/ralf.php
+++ dist/ralf.php
@@ -1,6 +1,14 @@
 <?php
 
-
+/**
+ * rafl Framework PHP
+ * Es un framework php diseñado para acelerar el desarrollo de una
+ * aplicación web escrita en php. Algunos de los componentes que soluciona
+ * este framework es rápida configuración de la base de datos, la opción
+ * de usar vistas para trabajar la interfaz gráficas.
+ *
+ * @author Jonathan Delgado Z <Jonad.correo@gmail.com>
+ */
 
 //Inicia la carga del framework
 include("lib/simplehtmldom/simple_html_dom.php");
