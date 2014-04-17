4f8afd79965f037927c2bc2db90cfe463a8e1f1c
Jonathan Delgado Zamorano <jonad.correo@gmail.com>
2014-04-16 23:41:26 -0400

    Publish Version 0.23.5

--- dist/ralf.php
+++ dist/ralf.php
@@ -1,29 +1,36 @@
 <?php 
-
+/**
+ * Website: https://github.com/alfa30/ralf-framework-php
+ *
+ * Ralf is a framework that facilitates the creation of software in php 
+ * by administration of free modules that facilitate the construction of
+ * an application. Ralf allows incorporation of external modules and 
+ * incorporates their own modules that facilitate the construction of 
+ * any application designed for web.
+ * 
+ * @author Jonathan Delgado Zamorano <jonad.correo@gmail.com>
+ * @version Prototype 0.23.5
+ * @package ralf
+ */
 if (class_exists("ralf") != true) {
 	class ralf
 	{
 		protected static $listImport = [];
 		protected $_pathFile = null;
-
 		function __construct() {
 			$this->_pathFile = dirname(__FILE__);
 		}
-
 		static function import($rute)
 		{
 			if (gettype($rute) == 'string') {
 				if (!in_array($rute, ralf::$listImport)) {
-
 					$pathSearch = dirname(__FILE__).'\\'.str_replace('.', '\\', $rute);
 					$pathSearch .= ".php";
-					
 					if (file_exists($pathSearch)) {
 						include $pathSearch;
 					} else {
 						throw new Exception("Seller does not exist");
 					}
-
 					ralf::$listImport[] = $rute;
 				}
 			} else {
